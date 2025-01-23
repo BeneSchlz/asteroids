eb1438f09f6e938a35ab6711c95388b40d19cbee
BeneSchlz <166176063+BeneSchlz@users.noreply.github.com>
2025-01-23 18:08:44 +0100

    screen & gameloop

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame
 from constants import *
 
+def game_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
 def main():
     pygame.init()
-    screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_loop()
+
 
 if __name__ == "__main__":
     main()
