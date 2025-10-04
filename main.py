429bb504548e87d7018cf77b2d6c22a3162c4efb
BeneSchlz <166176063+BeneSchlz@users.noreply.github.com>
2025-10-04 12:01:28 +0200

    Made Player appear on screen and added movement functionality on the player class to move wiht WASD

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
-    loop_switch = True
-    while loop_switch == True:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        #Limits Framerate to 60FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
